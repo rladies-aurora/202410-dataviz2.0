1a609255da030401f1ee0aab55ee37d0d3aef7d7
Janani Ravi <janani@msu.edu>
2020-02-11 10:05:10 -0500

    Update part4.R

--- part4.R
+++ part4.R
@@ -1,18 +1,19 @@
-#Dataviz2.0 - part4
-#install.packages("ggsci")
-#install.packages("ggsci")
-#install.packages("tidyverse")
-#install.packages("ggpubr")
-#install.packages("ggrepel")
-#install.packages("cowplot")
+## Dataviz2.0 - part4
+install.packages("ggsci")
+install.packages("ggsci")
+install.packages("tidyverse")
+install.packages("ggpubr")
+install.packages("ggrepel")
+install.packages("cowplot")
+install.packages("gridExtra")
 
-
-library("ggsci")
+library(ggsci)
 library(tidyverse)
 library(ggpubr)
 library(ggrepel)
 library(cowplot)
 library(ggrepel)
+library(gridExtra)
 
 #Create the data
 #We'll use the plots you've already created in part3: box1, ml_scatter, scatter2
@@ -43,21 +44,21 @@ head(gene_loc2)
 
 ##Combine boxplot and regression plot previously created
 ### Boxplot
-box1 <- ggplot(gene_loc2, aes(x = Chr, y = Length, 
+box1 <- ggplot(gene_loc2, aes(x = Chr, y = Length,
                               group=Chr, color=Chr)) +
   geom_boxplot() +
   xlab("Chr")+
   theme_bw()
 
 box1 <- box1 + scale_color_jco()+
-  theme(legend.position = "none")                 # remove legend            
+  theme(legend.position = "none")                 # remove legend
 #box1 + scale_fill_discrete(name = "Chromosome")  #rename legend
 
 ## Scatterplot + regression line
 ml_scatter <- ggscatter(gene_loc2, x = "log_EndStart", y = "log_length",
                         color = "Chr", palette = "jco",
                         add = "reg.line", add.params = list(color = "black"),    # customize regression line
-                        fullrange - TRUE) +   
+                        fullrange - TRUE) +
   facet_wrap(~Chr) +
   stat_cor(label.y = 4.4) +
   stat_regline_equation(label.y = 4.2)
@@ -67,7 +68,7 @@ ml_scatter
 ml_scatter <- ml_scatter + facet_grid(cols = vars(Chr))
 
 #Arranging boxplot and scatterplot on one page
-## we’ll use the function ggarrange()[in ggpubr], which is a wrapper around the function plot_grid() [in cowplot package]. 
+## we’ll use the function ggarrange()[in ggpubr], which is a wrapper around the function plot_grid() [in cowplot package].
 ## Compared to the standard function plot_grid(), ggarange() can arrange multiple ggplots over multiple pages.
 ## use either
 
@@ -82,7 +83,7 @@ fig
 
 annotate_figure(fig,
                              top = text_grob("Visualizing gene length", color = "red", face = "bold", size = 14),  #grob -- graphical object
-                             bottom = text_grob("Data source: \n gse69360 data set", color = "blue",         #customize text 
+                             bottom = text_grob("Data source: \n gse69360 data set", color = "blue",         #customize text
                                                 hjust = 1, x = 1, face = "italic", size = 10),             # horizontal justification: 0= left align, 1= right align
                              left = text_grob("Figure arranged using ggpubr", color = "green", rot = 90),   # rot = angle to rotate the text
                              right = "I'm done, thanks :-)!",
@@ -111,10 +112,10 @@ ggexport(final_plt, filename = "gene_lth_viz.pdf", width = 200, height = 200)
 # Place scatterplot together with density plot
 # Scatter plot colored by groups ("Chr")
 sp <- gene_loc2 %>%
-  ggscatter("log_EndStart", "log_length", 
+  ggscatter("log_EndStart", "log_length",
             color = "Chr", palette = "jco",
             size = 1, alpha = 0.2)+
-  border()                                         
+  border()
 # Marginal density plot of x (top panel) and y (right panel)
 xplot <- gene_loc2 %>%
   ggdensity("log_EndStart", fill = "Chr", palette = "jco")
@@ -123,18 +124,18 @@ yplot <- gene_loc2 %>%
   ggdensity("log_length", fill = "Chr", palette = "jco")+
   rotate()
 # Cleaning the plots
-yplot <- yplot + clean_theme() 
+yplot <- yplot + clean_theme()
 xplot <- xplot + clean_theme()
 # Arranging the plot
-fig_2 <- ggarrange(xplot, NULL, sp, yplot, 
-          ncol = 2, nrow = 2,  align = "hv", 
+fig_2 <- ggarrange(xplot, NULL, sp, yplot,
+          ncol = 2, nrow = 2,  align = "hv",
           widths = c(2, 1), heights = c(1, 2),
           common.legend = TRUE)
 
 # Annotating the plot
 scat_density_plt <- annotate_figure(fig_2,
                              top = text_grob("Scatter plot with marginal density", color = "red", face = "bold", size = 14),  #grob -- graphical object
-                             bottom = text_grob("Data source: \n gse69360 data set", color = "blue",         #customize text 
+                             bottom = text_grob("Data source: \n gse69360 data set", color = "blue",         #customize text
                                                 hjust = 1, x = 1, face = "italic", size = 10),             # horizontal justification: 0= left align, 1= right align
                              left = text_grob("Figure arranged using ggpubr", color = "green", rot = 90),   # rot = angle to rotate the text
                              right = "I'm done, thanks :-)!",
@@ -160,7 +161,7 @@ stable <- gene_loc2 %>%
               grps = "Chr")                       # Group
 stable <- stable[, c("Chr", "length", "mean", "sd")]
 # Summary table plot, medium blue theme
-stable.p <- ggtexttable(stable, rows = NULL, 
+stable.p <- ggtexttable(stable, rows = NULL,
                         theme = ttheme("mBlue"))
 
 # Arranging the plot
@@ -178,7 +179,7 @@ fig_4 <- scatter2 + annotation_custom(ggplotGrob(stable.p),
 # Annotating the plot
 final_scatter2 <- annotate_figure(fig_4,
                                   top = text_grob("Scatter plot with summary table", color = "red", face = "bold", size = 14),  #grob -- graphical object
-                                  bottom = text_grob("Data source: \n gse69360 data set", color = "blue",         #customize text 
+                                  bottom = text_grob("Data source: \n gse69360 data set", color = "blue",         #customize text
                                                      hjust = 1, x = 1, face = "italic", size = 10),             # horizontal justification: 0= left align, 1= right align
                                   left = text_grob("Figure arranged using ggpubr", color = "green", rot = 90),   # rot = angle to rotate the text
                                   right = "I'm done, thanks :-)!",
